da716c4c9cd55429b44bfb10ed995ae7b19193db
Babar-Hans <babarhans34@gmail.com>
2024-01-26 00:05:48 +0500

    Q3 has been completed

--- Q3.dart
+++ Q3.dart
@@ -1,3 +1,12 @@
+// Create a list of Days and remove one by one from the end of list.
 void main(){
-  print("Babar");
+  List<String> days = 
+["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+print(days);
+print("=================");
+while(days.isNotEmpty){
+  days.removeAt(0);
+  print(days);
+  }
+
 }
