b9ff89a8a1e297b459c375fc4fac85f1d63f6909
Babar-Hans <babarhans34@gmail.com>
2024-01-28 00:51:48 +0500

    some changes

--- Q5.dart
+++ Q5.dart
@@ -1,30 +1,30 @@
-// Create a map with name, phone keys and store some values to it. Use where to find all keys that have length 4.
-
-// void main(){
-  // Map<String,int> phone = {
-  //   "babar": 03161131987,
-  //   "Amir": 03161357911,
-  //   "Sami": 03150001010,
-  //   "Bilal": 0314920138
-  // };
-  // var phoneKeyLength = phone.keys.where((key)=>key.length==4);
-
-  // print(phoneKeyLength);
+// Create a map with name phone keys and store some values to it. Use where to find all keys that have length 4.
 
 void main(){
- Map<String,int> phone = {
+  Map<String,int> phone = {
     "babar": 03161131987,
     "Amir": 03161357911,
     "Sami": 03150001010,
     "Bilal": 0314920138
   };
+  var phoneKeyLength = phone.keys.where((key)=>key.length==4);
+  print(phoneKeyLength);
+}
+
+// void main(){
+//  Map<String,int> phone = {
+//     "babar": 03161131987,//1 while
+//     "Amir": 03161357911, //2 while
+//     "Sami": 03150001010, //3 while
+//     "Bilal": 0314920138 //4 while
+//   }; 
  
-while(phone.isNotEmpty){
-    var phoneKeyLength = phone.keys.where((key)=>key.length==4);
-    print(phoneKeyLength);
-    break;
-}
-}
+// while(phone.isNotEmpty){
+//     var phoneKeyLength = phone.keys.where((key)=>key.length == 4);
+//     print(phoneKeyLength);
+//     break;
+// }
+//}
 
 
 
