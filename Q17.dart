25cdab98e7b6584fb8408e23b2897e70f7722f19
Babar-Hans <babarhans34@gmail.com>
2024-01-29 17:24:19 +0500

    Q11  has been completed

---
+++ Q17.dart
@@ -0,0 +1,8 @@
+//Q.17: Given a list of integers, write a Dart code that uses the map() method to create a new list with each value squared. The program should take in the original list as a parameter and print the new list
+
+void main(){
+  List<int> values = [2,3,8,5,6];
+  List<int> squareValue = values.map((int v)=> v*v).toList();
+  print("Original List $values");
+  print("Squared List $squareValue");
+}
