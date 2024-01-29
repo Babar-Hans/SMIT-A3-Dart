8c79d70163d4dca6e29e0fc782d2cae2906a7fed
Babar-Hans <babarhans34@gmail.com>
2024-01-29 13:02:48 +0500

    Q7 has been completed

--- Q18.dart
+++ Q18.dart
@@ -1,10 +1,10 @@
 //Q.18: Create a map named "person" with the following key-value pairs: "name"as "John", "age" as 25, "isStudent" as true. Write a Dart code to check if the person is both a student and over 18 years old. Print "Eligible" if bothconditions are true, otherwise print "Not eligible".
 
 void main(){
-  Map<String,int> person = {
-  
-    
-  
+  Map<String,dynamic> person = {
+    "name": "Babar",
+    "age": 25,
+    "isStudent": true
 }
 
 }
